dd886c557ace01f139bf8a8604c2a3e261a77e33
Ricardo Mendes <ricardomlealjr@gmail.com>
2018-08-07 19:17:27 -0300

    Gerador

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Novas Tecnologias em Banco de Dados
 Trabalho da disciplina de Novas Tecnologias em Banco de Dados 2018/1
+
+### Inserindo os dados nos repectivos bancos
+* [mongoDB](https://zaiste.net/importing_json_into_mongodb/)
+* [redis](https://redis.io/topics/mass-insert)
+* Cassandra
---
+++ gen.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from pycpfcnpj import gen
+import random
+import names
+import json
+import sys
+
+def insert_string(pos):
+    return FILENAME[:pos] + '-mongo' + FILENAME[pos:], FILENAME[:pos] + '-redis' + FILENAME[pos:]
+
+
+if len(sys.argv) != 3:
+    print 'usage:'
+    print 'python redis-gen.py <file.txt> <number of registers>'
+    sys.exit()
+
+FILENAME = sys.argv[1]
+VALUES = int(sys.argv[2])
+
+produtos = [ 'geladeira', 'fogao', 'radio', 'laptop', 'celular',
+'televisao', 'ventilador', 'ar condicionado', 'maquina de lavar'
+]
+
+file_mongo, file_redis = insert_string(FILENAME.find('.txt'))
+
+f_redis = open(file_redis, 'w')
+f_mongo = open(file_mongo, 'w')
+print >> f_mongo, '['
+
+for i in range(1,VALUES):
+    obj = {}
+    obj['nome'] = names.get_full_name().encode('utf-8')
+    obj['cpf'] = gen.cpf_with_punctuation()
+    obj['carrinho'] = []
+    carrinho = {}
+
+    for j in range(1,3):
+        carrinho['produto'] = produtos[random.randint(0,8)]
+        carrinho['quantidade'] = random.randint(1,3)
+        carrinho['preco'] = round(random.uniform(200, 1000),2)
+
+        obj['carrinho'].append(carrinho)
+    
+    print >> f_redis, 'SET ' + str(i) + ' \'' + json.dumps(obj) + '\''
+    print >> f_mongo, '\t' + json.dumps(obj)
+
+print >> f_mongo, ']'
